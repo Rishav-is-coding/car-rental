83c5dbf76cb8a83a323e593ee4d180975f845cce
Rishav-is-coding <rishavraj02012003@gmail.com>
2024-07-07 22:24:23 +0530

    update 2

---
+++ src/components/Footer.jsx
@@ -0,0 +1,75 @@
+import { IconMail, IconPhoneCall } from "@tabler/icons-react";
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        <div className="container">
+          <div className="footer-content">
+            <ul className="footer-content__1">
+              <li>
+                <span>RK</span> Rental
+              </li>
+              <li>
+                We offers a big range of vehicles for all your driving needs. We
+                have the perfect vehicle to meet your needs.
+              </li>
+              <li>
+                <a href="tel:8638870746">
+                  <IconPhoneCall /> &nbsp; 8638870746
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="mailto: 
+                info@rk-rental.com"
+                >
+                  <IconMail />
+                  &nbsp; info@rk-rental.com
+                </a>
+              </li>
+            </ul>
+
+            <ul className="footer-content__2">
+              <li>Company</li>
+              <li>
+                <a href="https://maps.app.goo.gl/B1LDXo3vbV6FgbEj7" target="_blank">Guwahati</a>
+              </li>
+              <li>
+                <a href ="">Pricing Plans</a>
+              </li>
+              <li>
+                <a href="#home">Refunds/Cancellations</a>
+              </li>
+              <li>
+                <a href="#home">How we work</a>
+              </li>
+            </ul>
+
+            <ul className="footer-content__2">
+              <li>Working Hours</li>
+              <li>Mon - Sat: 9:00AM - 9:00PM</li>
+              <li>Sun: Closed</li>
+            </ul>
+
+            <ul className="footer-content__2">
+              <li>Subscription</li>
+              <li>
+                <p>Subscribe your Email address for latest news & updates.</p>
+              </li>
+              <li>
+                <input type="email" placeholder="Enter Email Address"></input>
+              </li>
+              <li>
+                <button className="submit-email">Submit</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
