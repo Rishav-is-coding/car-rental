4412f84d06eebb476ec0863ece9e4a6ed82ae86f
Rishav-is-coding <rishavraj02012003@gmail.com>
2024-09-03 15:33:00 +0530

    updated

--- src/Pages/Models.jsx
+++ src/Pages/Models.jsx
@@ -32,7 +32,7 @@ function Models() {
                       </span>
                     </div>
                     <div className="models-div__box__descr__name-price__price">
-                      <h4>$45</h4>
+                      <h4>Rs. 4500</h4>
                       <p>per day</p>
                     </div>
                   </div>
@@ -75,7 +75,7 @@ function Models() {
                       </span>
                     </div>
                     <div className="models-div__box__descr__name-price__price">
-                      <h4>$37</h4>
+                      <h4>Rs. 3700</h4>
                       <p>per day</p>
                     </div>
                   </div>
@@ -118,7 +118,7 @@ function Models() {
                       </span>
                     </div>
                     <div className="models-div__box__descr__name-price__price">
-                      <h4>$30</h4>
+                      <h4>Rs. 3000</h4>
                       <p>per day</p>
                     </div>
                   </div>
@@ -161,7 +161,7 @@ function Models() {
                       </span>
                     </div>
                     <div className="models-div__box__descr__name-price__price">
-                      <h4>$35</h4>
+                      <h4>Rs. 3500</h4>
                       <p>per day</p>
                     </div>
                   </div>
@@ -204,7 +204,7 @@ function Models() {
                       </span>
                     </div>
                     <div className="models-div__box__descr__name-price__price">
-                      <h4>$50</h4>
+                      <h4>Rs. 5000</h4>
                       <p>per day</p>
                     </div>
                   </div>
@@ -247,7 +247,7 @@ function Models() {
                       </span>
                     </div>
                     <div className="models-div__box__descr__name-price__price">
-                      <h4>$25</h4>
+                      <h4>Rs. 2500</h4>
                       <p>per day</p>
                     </div>
                   </div>
